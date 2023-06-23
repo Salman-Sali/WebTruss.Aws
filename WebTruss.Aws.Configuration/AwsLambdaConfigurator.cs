cc5e9a93fceac731b4161ac835bc42ec7875c63e
Salman-Sali <salman.s@flotillatechs.com>
2023-06-23 10:00:25 +0530

    Initial commit

---
+++ WebTruss.Aws.Configuration/AwsLambdaConfigurator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WebTruss.Aws.Configuration
+{
+    public class AwsLambdaConfigurator
+    {
+        public static T Bind<T>(T configurationModal, IConfiguration configuration)
+        {
+            var isLocal = configuration.GetValue<bool?>("Local");
+            if (isLocal.HasValue && isLocal.Value)
+            {
+                configuration.GetSection(typeof(T).Name).Bind(configurationModal);
+            }
+            else
+            {
+                foreach (var property in typeof(T).GetProperties())
+                {
+                    var value = configuration.GetValue(property.PropertyType, $"{typeof(T).Name}_{property.Name}");
+                    property.SetValue(configurationModal, value);
+                }
+            }
+            return configurationModal;
+        }
+    }
+}
